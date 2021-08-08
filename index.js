32ced73ee7c612799ec2169edb8d5d1a4ce8082f
Nora <nora5dempsey@gmail.com>
2021-08-08 10:49:25 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages
+}
+
+function countDown(number = 10) {
+    while (number >= 0) {
+        console.log(number--);
+    }
+}    
